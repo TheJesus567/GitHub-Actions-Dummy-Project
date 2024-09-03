1c747b733635de20238f78c3bb29a8f700ca9e02
Jesús Iván Lemus Cervantes <thejesus567@gmail.com>
2024-09-03 15:18:21 -0600

    added log to debugging

--- data/database.js
+++ data/database.js
@@ -16,7 +16,8 @@ try {
   await client.db(dbName).command({ ping: 1 });
   console.log('Connected successfully to server');
 } catch (error) {
-  console.log('Connection failed.');
+  console.log(error)
+  console.log('Connection failed.'+ uri);
   await client.close();
   console.log('Connection closed.');
 }
